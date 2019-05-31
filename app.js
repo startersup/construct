f4c193fee325295fa7f3f95512fdef4f17351792
vinodhan16 <vinodhan.thunderbolt@gmail.com>
2019-06-01 01:12:20 +0530

    Node conversion started

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var ejs = require('ejs');
+var path = require('path');
+var app = express();
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname+'/assets')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.get('/index',function (req,res) {  
+    res.redirect('/') ;
+});
+app.get('/',function (req,res) {  
+    console.log("Lets start");
+    res.render('index') ;
+});
+app.get('/task',function (req,res) {  
+   res.render('task');
+});
+app.get('/bills',function (req,res) {  
+    res.render('bills');
+ });
+ app.get('/employees',function (req,res) {  
+    res.render('empdata');
+ });
+ console.log("Listening on 3000");
+app.listen(3000);
