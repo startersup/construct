082709476d63241b70f228508a292d201d178069
saicharan96 <saicharan14996@gmail.com>
2019-10-26 16:07:47 +0530

    push it
    push it

--- server/routes/mobile.routes.js
+++ server/routes/mobile.routes.js
@@ -1,4 +1,10 @@
 module.exports = function(router,app){
+  router.get('/mobile/mobiledash', function(req, res) {
+    res.render('./mobile/mobiledash');
+  });
+  router.get('/mobile/next', function(req, res) {
+    res.render('./mobile/next');
+  });
     router.get('/mobile/login', function(req, res) {
         res.render('./mobile/login');
       });
