f5ad551d7f5cdb2e9c3df67802222a2215957070
Ubuntu <ubuntu@ip-172-31-42-44.ap-south-1.compute.internal>
2019-10-30 19:39:58 +0000

    Merge Resolve

--- server/boot/data-population.js
+++ server/boot/data-population.js
@@ -5,36 +5,43 @@ module.exports= async function(app){
   if(dbWork.populated==true){
     console.log("----->Modules Already Populated");
     return;
-  }
-    var User = app.models.UserInfo;
-    var Role = app.models.Role;
-    var RoleMapping = app.models.RoleMapping;
-   await User.create([
-      {first_name: 'Super Admin', email: 'saicharan14996@gmail.com', password: 'admin123',phone:'8056012353'}
-    ], async function(err, users) {
-        console.log('----->>>Users Populated');
-        await Role.create([{name: 'Super Admin'},{name: 'Admin'},{name: 'Site Admin'},{name: 'Worker'}], async function(err, roles) {
-        console.log('----->>>Roles Populated');
-       await roles[0].principals.create({
-          principalType: RoleMapping.USER,
-          principalId: users[0].id
-        }, function(err, principal) {
-          console.log('----->>>Super Admin Added');
+  }else{
+      var User = app.models.UserInfo;
+      var Role = app.models.Role;
+      var RoleMapping = app.models.RoleMapping;
+    await User.create([
+        {first_name: 'Super Admin', email: 'saicharan14996@gmail.com', password: 'admin123',phone:'8056012353'}
+      ], async function(err, users) {
+          console.log('----->>>Users Populated');
+          await Role.create([{name: 'Super Admin'},{name: 'Admin'},{name: 'Site Admin'},{name: 'Worker'}], async function(err, roles) {
+          console.log('----->>>Roles Populated');
+        await roles[0].principals.create({
+            principalType: RoleMapping.USER,
+            principalId: users[0].id
+          }, function(err, principal) {
+            console.log('----->>>Super Admin Added');
+          });
         });
       });
-    });
-    var POStatus = app.models.PurchaseStatus;
-    var status = [{name:"Draft",is_pending:1},{name:"Requested",is_pending:1},{name:"Confirmed",is_pending:1},{name:"Partially Recieved",is_pending:1},{name:"Recieved",is_pending:0},{name:"Cancelled",is_pending:0}];
-    await POStatus.create(status,function(err,status){
+      var POStatus = app.models.PurchaseStatus;
+      var status = [{name:"Draft",is_pending:1},{name:"Requested",is_pending:1},{name:"Confirmed",is_pending:1},{name:"Partially Recieved",is_pending:1},{name:"Recieved",is_pending:0},{name:"Cancelled",is_pending:0}];
+      await POStatus.create(status,function(err,status){
+          if (err) throw err;
+          console.log("----->>>PO Status Populated");
+      });
+      var requestStatus = app.models.RequestStatus;
+      status = [{name:"Open",is_pending:1},{name:"On Hold",is_pending:1},{name:"In Progress",is_pending:1},{name:"Closed",is_pending:0}]
+      await requestStatus.create(status,function(err,status){
         if (err) throw err;
-        console.log("----->>>PO Status Populated");
+        console.log("----->>>Request Status Populated");
     });
-    var requestStatus = app.models.RequestStatus;
-    status = [{name:"Open",is_pending:1},{name:"On Hold",is_pending:1},{name:"In Progress",is_pending:1},{name:"Closed",is_pending:0}]
-    await requestStatus.create(status,function(err,status){
+    var priorityDef = app.models.PriorityDefinition;
+    var priority = [{name:"High",color:1},{name:"Low",color:1},{name:"Normal",color:1},{name:"Immediate",color:0}]
+    await priorityDef.create(priority,function(err,priority){
       if (err) throw err;
-      console.log("----->>>Request Status Populated");
+      console.log("----->>>Priority Definition Populated");
   });
+<<<<<<< HEAD
   var priorityDef = app.models.PriorityDefinition;
   var priority = [{name:"High",color:1},{name:"Low",color:1},{name:"Normal",color:1},{name:"Immediate",color:0}]
   await priorityDef.create(priority,function(err,priority){
@@ -50,3 +57,15 @@ module.exports= async function(app){
     dbWork.populated=true;
     fs.writeFileSync(path.join(__dirname, '/../db-work.json'), JSON.stringify(dbWork));
 }
+=======
+      var Inventory = app.models.Inventory;
+      var InventoryData = require("../data-dictionary/product-dictionary.json");
+      await Inventory.create(InventoryData,function(err,data){
+          if (err) throw err;
+          console.log("----->>>Inventory Products Populated");
+      });
+      dbWork.populated=true;
+      fs.writeFileSync(path.join(__dirname, '/../db-work.json'), JSON.stringify(dbWork));
+  }
+}
+>>>>>>> 8c7e81c32d66b9daf8010e460ad8cad2e25cc5b6
