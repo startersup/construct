6d18ad967997059768a34bd475b529e440b15845
saicharan96 <saicharan14996@gmail.com>
2019-11-08 08:22:05 +0530

    minor commit
    minor commit

--- server/routes/mobile.routes.js
+++ server/routes/mobile.routes.js
@@ -2,13 +2,13 @@ module.exports = function(router,app){
   router.get('/mobile/mobiledash', function(req, res) {
     res.render('./mobile/mobiledash');
   });
-  router.get('/mobile/next', function(req, res) {
-    res.render('./mobile/next');
-  });
   router.get('/mobile/irequest', function(req, res) {
     res.render('./mobile/irequest');
   });
     router.get('/mobile/login', function(req, res) {
         res.render('./mobile/login');
+      });
+      router.get('/mobile/next', function(req, res) {
+        res.render('./mobile/next');
       });
 }
